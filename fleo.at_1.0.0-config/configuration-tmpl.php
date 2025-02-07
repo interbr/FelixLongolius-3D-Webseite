0d13aed55e1bd9f69a98204491cb5b2105ebc4d9
interbr <felix@weltpolizei.de>
2025-02-07 00:53:16 +0000

    further prepare step-out-version

---
+++ fleo.at_1.0.0-config/configuration-tmpl.php
@@ -0,0 +1,15 @@
+<?php
+$sitename = "";
+$siteurl = ""; // without "https://" and without trailing slash
+$fleoPathAbs = "";
+$dbServer = "";
+$db = "";;
+$dbuser = "";
+$dbpw = "";
+$mailpassword = "";
+$mailrecipient = "";
+$mailhost = "";
+$mailusername = "";
+$mailfromline = "";
+$whoAllowedUpload = 2;
+$openAiKey = "";
