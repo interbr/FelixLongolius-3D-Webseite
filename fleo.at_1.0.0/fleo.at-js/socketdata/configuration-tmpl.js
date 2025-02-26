358121b744912ffc70e81528e79ff3bb47e72631
interbr <felix@weltpolizei.de>
2025-02-26 10:12:24 +0000

    code-cleanup, beautify, minify for 1.0.0 (and history.replaceState instead of pushState)

--- fleo.at_1.0.0/fleo.at-js/socketdata/configuration-tmpl.js
+++ fleo.at_1.0.0/fleo.at-js/socketdata/configuration-tmpl.js
@@ -2,3 +2,7 @@ const nodeDb_host = "";
 const nodeDb_user = "";
 const nodeDb_password = "";
 const nodeDb_database = "";
+
+module.exports = {
+    nodeDb_host, nodeDb_user, nodeDb_password, nodeDb_database
+  };
